a64d479430b9665a6031fe967a77e2ecb0fa8a2a
markn <markn@aesclever.com>
2023-08-03 23:17:53 -0700

    Add project files.

---
+++ Models/ModelBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using HPlussSport.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace HPlusSport.API.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder ) 
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Active Wear - Men" },
+                new Category { Id = 2, Name = "Active Wear - Women" },
+                new Category { Id = 3, Name = "Mineral Water" },
+                new Category { Id = 4, Name = "Publications" },
+                new Category { Id = 5, Name = "Supplements" }
+                );
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, CategoryId = 1, Name = "Jogstrap" },
+                new Product { Id = 2, CategoryId = 1, Name = "Baseball cap" },
+                new Product { Id = 3, CategoryId = 2, Name = "Sneaker" },
+                new Product { Id = 4, CategoryId = 2, Name = "Headband" },
+                new Product { Id = 5, CategoryId = 2, Name = "Support bra" },
+                new Product { Id = 6, CategoryId = 3, Name = "Electrolite" },
+                new Product { Id = 7, CategoryId = 3, Name = "Gatorate" },
+                new Product { Id = 8, CategoryId = 4, Name = "Chi Running" },
+                new Product { Id = 9, CategoryId = 4, Name = "Tour De France" },
+                new Product { Id = 10, CategoryId = 5, Name = "Seaweed Bar" },
+                new Product { Id = 11, CategoryId = 5, Name = "B12 Xperformance" },
+                new Product { Id = 12, CategoryId = 5, Name = "BioBoostTonage" }
+                );
+                
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Building Web APIs with ASP.NET Core in .NET 
+
+*Development environment: Visual Studio 2022
+
+Language: C-Sharp
+
+References: LinkedIn Learning by Christian Wenz* 
+
+
+<hr />
+
+**REST with HTTP Methods (CRUD)**
+
+| HTTP Methods  | Function  |
+|---|---|
+| GET  | Read data  |
+| POST  | Create new data  |
+| PUT  | Update existing data  |
+| DELETE  | Delete existing data |
+
+
+
+
+### Runtime output
+
+![](.\SwaggerOutput.PNG)
+
+
+
+
+### Additional References:
+
+[Hshopsport](https://hplussport.com/shop/)
+
+
